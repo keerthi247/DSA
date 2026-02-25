68d302f43c5c883d02723dba1c3d6366fc03c2f1
keerthi s <keerthugowda2004@gmail.com>
2026-02-25 18:19:12 +0530

    Implement factorial function using recursion

---
+++ recursion.py
@@ -0,0 +1,7 @@
+def factorial(n):
+    if n == 0 or n == 1:   # Base case
+        return 1
+    return n * factorial(n-1)   # Recursive call
+
+num = int(input("Enter number: "))
+print("Factorial =", factorial(num))
